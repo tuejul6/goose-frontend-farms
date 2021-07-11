b6b91efc556c5e3c2541f8194ddc7c7bb7633fb0
tuejul6 <87008121+tuejul6@users.noreply.github.com>
2021-07-12 00:46:16 +0700

    frontpage lang

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -60,9 +60,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'Goose Finance')}
+          {TranslateString(576, 'HOMER Finance')}
         </Heading>
-        <Text>{TranslateString(578, 'Top 3 best DEFI app on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'Print your own Money !')}</Text>
       </Hero>
       <div>
         <Cards>
