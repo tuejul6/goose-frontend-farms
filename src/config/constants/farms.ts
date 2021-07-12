30da4cbe2e57d4435209ed469e4a42693f923e21
tuejul6 <87008121+tuejul6@users.noreply.github.com>
2021-07-12 17:57:51 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -18,7 +18,38 @@ const farms: FarmConfig[] = [
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd,
   },
-  
+  {
+    pid: 1,
+    risk: 5,
+    lpSymbol: 'BNB-GIRL LP',
+    lpAddresses: {
+      97: '0x11cdbbd21bbe35a244806dfcad97135ae8f14a4d',
+      56: '',
+    },
+    tokenSymbol: 'GIRL',
+    tokenAddresses: {
+      97: '0x3b8EC572e56Cf7Ad299494f28488d1dFA4F0B933',
+      56: '',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
+    pid: 2,
+    risk: 3,
+    lpSymbol: 'BNB-BUSD LP',
+    lpAddresses: {
+      97: '0xe0e92035077c39594793e61802a350347c320cf2',
+      56: '',
+    },
+    tokenSymbol: 'BNB',
+    tokenAddresses: {
+      97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
+      56: '',
+    },
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
+  },
 
 ]
 
