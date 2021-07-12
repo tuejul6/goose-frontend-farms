609488e1d45551016065fc5c4b77b8db78961edd
tuejul6 <87008121+tuejul6@users.noreply.github.com>
2021-07-12 17:33:25 +0700

    Update farms.ts
    Switch to testnet 97

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -7,13 +7,13 @@ const farms: FarmConfig[] = [
     risk: 5,
     lpSymbol: 'GIRL-BUSD LP',
     lpAddresses: {
-      97: '',
-      56: '0x98c07519193604126d6593059e6933b1a6e5d36b',
+      97: '0x98c07519193604126d6593059e6933b1a6e5d36b',
+      56: '',
     },
     tokenSymbol: 'GIRL',
     tokenAddresses: {
-      97: '',
-      56: '0x3b8EC572e56Cf7Ad299494f28488d1dFA4F0B933',
+      97: '0x3b8EC572e56Cf7Ad299494f28488d1dFA4F0B933',
+      56: '',
     },
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd,
